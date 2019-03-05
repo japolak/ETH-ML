fa99d5220e4e9a573216c4772f49d2f1485abd60
Jakub <Jakub@Jakubs-MacBook-Pro.local>
2019-03-05 17:19:10 +0100

    update #1 (rm unnecessary package list, printout text)

--- main0.py
+++ main0.py
@@ -12,7 +12,6 @@ import numpy as np
 import pandas as pd
 import warnings
 from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import Imputer
 from sklearn.model_selection import train_test_split
 
 #%% Select directory
